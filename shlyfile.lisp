47b297cf57e8dc2033c30dcd805e09b5d76c3e57
Lilas Dinh <Lvkdinh@gmail.com>
2015-09-18 17:04:18 -0400

    shly you nust be joking

---
+++ shlyfile.lisp
@@ -0,0 +1,26 @@
+;; -*- mode: common-lisp -*-
+
+(load "patriothackers-cl.asd")
+(load "postmodern-localtime.asd")
+
+(ql:quickload '(:patriothackers-cl :split-sequence))
+
+(import 'split-sequence:split-sequence)
+
+#+sbcl(setf sb-impl::*default-external-format* :utf-8)
+#+sbcl(setf sb-alien::*default-c-string-external-format* :utf-8)
+
+(defun start (&key (port 50000)
+                   (server :wookie)
+                   (debug nil))
+  (flet ((start (&rest args)
+           (apply #'patriothackers-cl:start
+                  :debug debug :server server args)))
+    (let ((server-starter-port (asdf::getenv "SERVER_STARTER_PORT")))
+      (if server-starter-port
+          (destructuring-bind (port fd)
+              (split-sequence #\=
+                              (car (split-sequence #\; server-starter-port :count 1)))
+            (start :port (parse-integer port)
+                   :fd (parse-integer fd)))
+          (start :port port)))))
